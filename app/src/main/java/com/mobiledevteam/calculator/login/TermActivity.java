392133b51e0c6bf3e6d8641fe08fe99170c2aa76
MinJerry1129 <jinxiancheng1129@hotmail.com>
2021-03-25 23:48:45 +0800

    First commit

---
+++ app/src/main/java/com/mobiledevteam/calculator/login/TermActivity.java
@@ -0,0 +1,16 @@
+package com.mobiledevteam.calculator.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mobiledevteam.calculator.R;
+
+public class TermActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_term);
+    }
+}
