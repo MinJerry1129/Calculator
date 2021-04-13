52ad6ac5763523ad2cf316fe7c5d320dec973e5e
MinJerry1129 <jinxiancheng1129@hotmail.com>
2021-04-13 15:40:04 +0800

    update design

--- app/src/main/java/com/mobiledevteam/calculator/Utils/Common.java
+++ app/src/main/java/com/mobiledevteam/calculator/Utils/Common.java
@@ -8,10 +8,22 @@ public class Common {
     private static Common instance = new Common();
 //    private String baseURL = "http://phpstack-362651-1717329.cloudwaysapps.com/";
     private String baseURL = "http://10.0.2.2/proderma/";
-    private ArrayList<Category> mCategory;
+    private ArrayList<Category> mIncomeCategory;
+    private ArrayList<Category> mLiabilityCategory;
+    private ArrayList<String> mRepeatCategory;
+
 
     public static Common getInstance() {
         return instance;
     }
     public String getBaseURL() {return baseURL;}
+
+    public ArrayList<Category> getmIncomeCategory() { return mIncomeCategory;}
+    public void setmIncomeCategory(ArrayList<Category> mIncomeCategory) { this.mIncomeCategory = mIncomeCategory; }
+
+    public ArrayList<Category> getmLiabilityCategory() { return mLiabilityCategory;}
+    public void setmLiabilityCategory(ArrayList<Category> mLiabilityCategory) { this.mLiabilityCategory = mLiabilityCategory; }
+
+    public ArrayList<String> getmRepeatCategory() { return mRepeatCategory;}
+    public void setmRepeatCategory(ArrayList<String> mRepeatCategory) { this.mRepeatCategory = mRepeatCategory;}
 }
--- app/src/main/java/com/mobiledevteam/calculator/login/LoginActivity.java
+++ app/src/main/java/com/mobiledevteam/calculator/login/LoginActivity.java
@@ -9,9 +9,16 @@ import android.widget.Button;
 
 import com.mobiledevteam.calculator.MainActivity;
 import com.mobiledevteam.calculator.R;
+import com.mobiledevteam.calculator.Utils.Common;
+import com.mobiledevteam.calculator.cell.Category;
+
+import java.util.ArrayList;
 
 public class LoginActivity extends AppCompatActivity {
     private Button _btnSign;
+    private ArrayList<Category> mIncomeCategory;
+    private ArrayList<Category> mLiabilityCategory;
+    private ArrayList<String> mRepeatCategort;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,36 @@ public class LoginActivity extends AppCompatActivity {
         setReady();
     }
     private void setReady(){
+        mIncomeCategory.add(new Category("1","Salary","salary"));
+        mIncomeCategory.add(new Category("2","Business Profit","business"));
+        mIncomeCategory.add(new Category("3","Project","project"));
+        mIncomeCategory.add(new Category("4","Royalty","royalty"));
+        mIncomeCategory.add(new Category("5","Other","other"));
+
+        mLiabilityCategory.add(new Category("1","Condo Fee","condo"));
+        mLiabilityCategory.add(new Category("2","Property Taxes","property"));
+        mLiabilityCategory.add(new Category("2","Mortgage Payment","mortgage"));
+        mLiabilityCategory.add(new Category("2","Cell Phone","phone"));
+        mLiabilityCategory.add(new Category("2","Car Gas","gas"));
+        mLiabilityCategory.add(new Category("2","Car Insurance","insurance"));
+        mLiabilityCategory.add(new Category("2","Electric Bill","electric"));
+        mLiabilityCategory.add(new Category("2","Groceries","groceries"));
+        mLiabilityCategory.add(new Category("2","Gas/Heating","heating"));
+        mLiabilityCategory.add(new Category("2","Tv/Internet","tv"));
+        mLiabilityCategory.add(new Category("2","Water","water"));
+        mLiabilityCategory.add(new Category("2","Secured Line of Credit","secured"));
+
+        mRepeatCategort.add("Once");
+        mRepeatCategort.add("Every Day");
+        mRepeatCategort.add("Every Week");
+        mRepeatCategort.add("Every Month");
+        mRepeatCategort.add("Semi Anually");
+        mRepeatCategort.add("Anually");
+
+        Common.getInstance().setmIncomeCategory(mIncomeCategory);
+        Common.getInstance().setmLiabilityCategory(mLiabilityCategory);
+        Common.getInstance().setmRepeatCategory(mRepeatCategort);
+
         _btnSign.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
